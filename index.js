d5c205ead395b609f80caed309be2d98fe902282
ahmadrosid <alahmadrosid@gmail.com>
2020-07-21 15:25:17 +0700

    feat: add lightstep extracted context on attributes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { v4 }  = require('uuid');
 const { PubSub } = require("@google-cloud/pubsub");
 var opentracing = require('opentracing');
 var lightstep   = require('lightstep-tracer');
@@ -6,11 +7,7 @@ var lightstep   = require('lightstep-tracer');
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = 3000;
-
-const ID = function () {
-	return Math.random().toString(36).substr(2, 60);
-};
+const port = 3001;
 
 const env = (variable) => {
 	return process.env[variable];
@@ -25,7 +22,10 @@ opentracing.initGlobalTracer(tracer);
 
 app.post("/", async (req, res) => {
 	const span = opentracing.globalTracer().startSpan('pubsubEvent');
-	const tracsactionID = ID();
+	const tracsactionID = v4();
+	let spanContext = {}
+	opentracing.globalTracer().inject(span.context(), opentracing.FORMAT_TEXT_MAP, spanContext);
+
 	span.setTag('kind', 'pubsubEvent');
 	span.setTag('guid:transaction_id', tracsactionID);
 
@@ -37,7 +37,7 @@ app.post("/", async (req, res) => {
 	}
 
 	span.log({ event: 'Start publish pubsub message' })
-	await publish(data, topicName, tracsactionID)
+	await publish(data, topicName, tracsactionID, JSON.stringify(spanContext))
 	.then((messageId) => {
 		span.log({ event: 'Finish publish pubsub message' })
 		res.send(
@@ -57,13 +57,16 @@ app.post("/", async (req, res) => {
 
 app.listen(port, () => console.log(`Server run at http://localhost:${port}`));
 
-async function publish(input, topicName, tracsactionID) {
+async function publish(input, topicName, tracsactionID, spanContext) {
   const data = JSON.stringify(input);
   const pubSubClient = new PubSub();
 
   async function publishMessage() {
 	const dataBuffer = Buffer.from(data);
-	const attributes = { lsTransactionId: tracsactionID };
+	const attributes = { 
+		transactionId: tracsactionID,
+		spanContext: spanContext
+	};
 
     return await pubSubClient.topic(topicName).publish(dataBuffer, attributes);
   }
